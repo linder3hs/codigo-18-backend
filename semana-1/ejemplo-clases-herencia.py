fc4062e862d32cfc8bc278a3544adeedfe7ecb62
Linder Hassinger <linder@peppermint.pe>
2024-05-17 22:18:35 -0500

    ➕ feat: poo

--- semana-1/ejemplo-clases-herencia.py
+++ semana-1/ejemplo-clases-herencia.py
@@ -1,13 +1,21 @@
 class Persona:
     def __init__(self, nombre, apellido, altura=None, peso=None, edad=None):
-        self.nombre = nombre
+        self.__nombre = nombre
         self.apellido = apellido
         self.edad = edad
         self.altura = altura
         self.peso = peso
 
     def saludar(self):
-        return f"Hola me llamo {self.nombre} {self.apellido}"
+        return f"Hola me llamo {self.__nombre} {self.apellido}"
+
+    # setter
+    def modificar_nombre(self, nuevo_nombre):
+        self.__nombre = nuevo_nombre
+
+    # getter
+    def obtener_nombre(self):
+        return self.__nombre
 
 
 class Estudiante(Persona):
@@ -16,9 +24,10 @@ class Estudiante(Persona):
         self.carrera = carrera
 
     def obtener_carrera(self):
-        return f"Hola me llamo {self.nombre} y estudio {self.carrera}"
+        return f"Hola me llamo {self.obtener_nombre()} y estudio {self.carrera}"
 
 
 estudiante1 = Estudiante("Pepe", "Perez", 29, "Ing. Software")
+estudiante1.modificar_nombre("Pepe2")
 print(estudiante1.saludar())
 print(estudiante1.obtener_carrera())
