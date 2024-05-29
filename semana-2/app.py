af2459db36bb2621e84c168d66bc56e142bc1fab
Linder Hassinger <linder@peppermint.pe>
2024-05-28 20:44:17 -0500

    ➕ feat: list

--- semana-2/app.py
+++ semana-2/app.py
@@ -17,6 +17,45 @@ def home():
     return jsonify([])
 
 
+# GET
+@app.route('/api/v1/user')
+def get_all_users():
+    try:
+        users = User.query.all()
+
+        dic_users = []
+
+        for user in users:
+            dic_users.append(user.to_dic())
+
+        return jsonify({
+            'users': dic_users
+        })
+    except Exception as e:
+        return jsonify({
+            "error": e,
+            "linea": e.__traceback__.tb_lineno
+        }), 500
+
+
+# GET by user id
+@app.route('/api/v1/user/<int:user_id>')
+def get_user_by_id(user_id):
+    try:
+        # buscamos al usuario porid
+        user = User.query.get(user_id).to_dic()
+
+        return jsonify({
+            "user": user
+        })
+    except Exception as e:
+        return jsonify({
+            "error": e,
+            "linea": e.__traceback__.tb_lineno
+        }), 500
+
+
+# POST
 @app.route('/api/v1/user', methods=['POST'])
 def create_user():
     try:
@@ -31,17 +70,18 @@ def create_user():
             phoneNumber=user_data['phone_number'],
             genre=user_data['genre']
         )
+
         db.session.add(new_user)
-        db.session.commit()
+        db.session.commit()  # COMMIT (SAVEPOINT)
 
         return jsonify({
             "new_user": new_user.to_dic()
-        })
+        }), 201
     except Exception as e:
         return jsonify({
             "error": e,
             "linea": e.__traceback__.tb_lineno
-        })
+        }), 500
 
 
 if __name__ == '__main__':
