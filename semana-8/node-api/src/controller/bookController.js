4089ae60263f6f8b89bfde14b913eca8ae3401d6
linder3hs <linderhassinger00@gmail.com>
2024-07-05 22:19:47 -0500

    crud folders

--- semana-8/node-api/src/controller/bookController.js
+++ semana-8/node-api/src/controller/bookController.js
@@ -22,3 +22,50 @@ async function getBookById(req, res) {
     book,
   });
 }
+
+async function createBook(req, res) {
+  const book = req.body;
+
+  const newBook = await prisma.book.create({
+    data: {
+      title: book.title,
+      author: book.author,
+      summary: book.summary,
+      isbn: book.isbn,
+      is_published: book.is_published,
+      published_date: new Date(book.published_date),
+    },
+  });
+
+  return res.status(201).json({
+    book: newBook,
+  });
+}
+
+async function updateBook(req, res) {
+  const bookUpdated = await prisma.book.update({
+    where: {
+      id: Number(req.params.id),
+    },
+    // es la informacion que vamos a actualizar req.body
+    data: req.body,
+  });
+
+  return res.json({
+    book: bookUpdated,
+  });
+}
+
+async function deleteBook(req, res) {
+  const bookDeleted = await prisma.book.delete({
+    where: {
+      id: Number(req.params.id),
+    },
+  });
+
+  return res.json({
+    book: bookDeleted,
+  });
+}
+
+module.exports = { getBooks, getBookById, createBook, updateBook, deleteBook };
