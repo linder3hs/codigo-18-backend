47ca8211c61e37510ad791da8653aa518cbea3c4
linder3hs <linderhassinger00@gmail.com>
2024-07-11 22:14:23 -0500

    ➕ feat: fix aws-sdk/s3-client

--- semana-9/node-api-ts/src/config/s3.ts
+++ semana-9/node-api-ts/src/config/s3.ts
@@ -1,14 +1,14 @@
-import AWS from "aws-sdk";
 import dotenv from "dotenv";
+import { S3Client } from "@aws-sdk/client-s3";
 
 dotenv.config();
 
-AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+const s3 = new S3Client({
   region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY as string,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
+  },
 });
-
-const s3 = new AWS.S3();
 
 export default s3;
---
+++ semana-9/node-api-ts/src/config/upload.ts
@@ -0,0 +1,17 @@
+import multer from "multer";
+import multerS3 from "multer-s3";
+import s3 from "./s3";
+import type { Request } from "express";
+
+const upload = multer({
+  storage: multerS3({
+    s3,
+    bucket: process.env.AWS_S3_BUCKET_NAME as string,
+    acl: "public-read",
+    metadata: (req: Request, file, cb) => {
+      cb(null, {
+        fieldName: file.fieldname,
+      });
+    },
+  }),
+});
