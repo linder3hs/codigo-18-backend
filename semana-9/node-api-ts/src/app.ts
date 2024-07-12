939ec896ba9c80104387542f91595b26ceed6cf7
linder3hs <linderhassinger00@gmail.com>
2024-07-11 21:43:43 -0500

    aws s3

--- semana-9/node-api-ts/src/app.ts
+++ semana-9/node-api-ts/src/app.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import express from "express";
 import homeRouter from "./routes/homeRouter";
 import bookRouter from "./routes/bookRouter";
---
+++ semana-9/node-api-ts/src/config/s3.ts
@@ -0,0 +1,14 @@
+import AWS from "aws-sdk";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+});
+
+const s3 = new AWS.S3();
+
+export default s3;
